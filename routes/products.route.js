5c5064eb168541ecfdafd5ee07f75f938da6c5f9
martinsalinas0 <martinlsalinas@pm.me>
2025-05-24 21:18:24 -0500

    GET all products w/pagination

--- routes/products.route.js
+++ routes/products.route.js
@@ -1,24 +1,21 @@
 const router = require("express").Router();
 const Product = require("../models/product");
 
-router.get('/products', async (req, res) => { 
+router.get("/products", async (req, res) => {
   try {
-    
     const perPage = 9; //products per page
-    const page = (req.query.page) || 1; //page requested, and set DEFAULT to page 1 
+    const page = req.query.page || 1; //page requested, and set DEFAULT to page 1
 
     const products = await Product.find()
-    .skip((page - 1) * perPage)
-    .limit(perPage)
-
+      .skip((page - 1) * perPage)
+      .limit(perPage);
 
     const count = await Product.countDocuments(); //number on the page
 
-    res.status(200).json({products: products })
-
+    res.status(200).json({ products: products });
   } catch (error) {
-    res.status(500).json({message: error.message})
+    res.status(500).json({ message: error.message });
   }
-})
+});
 
 module.exports = router;
