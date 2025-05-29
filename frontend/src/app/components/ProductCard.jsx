d8a099a892db87e1c2e7cfd0c87723d900cb951b
martinsalinas0 <martinlsalinas@pm.me>
2025-05-28 21:32:22 -0500

    edited the props in productCard.jsx

--- frontend/src/app/components/ProductCard.jsx
+++ frontend/src/app/components/ProductCard.jsx
@@ -7,30 +7,65 @@ import {
   CardText,
   CardTitle,
   Container,
+  Row,
+  Col,
 } from "react-bootstrap";
 import { FaTrash } from "react-icons/fa";
+import { AiOutlineProduct } from "react-icons/ai";
+import { BsFillTrash3Fill, BsInfoSquare } from "react-icons/bs";
 
-export default function ProductCard( {name, price, category, imageUrl}) {
+export default function ProductCard({ name, price, category, imageUrl }) {
+  const capitalize = (str) => {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  };
+
+  const priceFixer = (price) => {
+    const timesTen = price / 10;
+    return timesTen.toFixed(2);
+  };
+
   return (
     <Card style={{ width: "18rem" }}>
       <CardBody>
-        <CardTitle>PRODUCT NAME  /*name*/</CardTitle>
-        <CardText>This is the product text</CardText>
-        <CardSubtitle className="mb-2 text-muted">Price: $3.22   /*price*/</CardSubtitle>
+        <CardTitle>{name}</CardTitle>
         <CardSubtitle className="mb-3 text-muted">
-          Category: Garden {/*category*/}
+          Category: {capitalize(category)}
+        </CardSubtitle>
+        <CardSubtitle className="mb-3 text-muted">
+          Price: ${priceFixer(price)}
         </CardSubtitle>
 
-        <CardImg
-          src="https://picsum.photos/200" /*imageUrl*/
-          style={{ width: "250px", height: "300px", objectFit: "cover" }}
-        />
+        {/* Centered image */}
+        <div className="d-flex justify-content-center mb-3">
+          <CardImg
+            src={imageUrl}
+            style={{ width: "250px", height: "300px", objectFit: "cover" }}
+          />
+        </div>
 
-        <Container className="d-flex justify-content-end p-0">
-          <Button variant="danger" size="sm">
-            <FaTrash />
-          </Button>
-        </Container>
+        <CardText className="text-center">This is the product text</CardText>
+        <Row className="g-2">
+          <Col>
+            <Button
+              className="w-100"
+              variant="primary"
+              size="sm"
+              aria-label="go to product page"
+            >
+              <BsInfoSquare />
+            </Button>
+          </Col>
+          <Col>
+            <Button
+              className="w-100"
+              variant="danger"
+              size="sm"
+              aria-label="Delete product"
+            >
+              <BsFillTrash3Fill />
+            </Button>
+          </Col>
+        </Row>
       </CardBody>
     </Card>
   );
