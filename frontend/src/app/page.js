5077701bb4f192a611a3f7ffcbb85cf7ce6e0b92
martinsalinas0 <martinlsalinas@pm.me>
2025-06-01 12:48:12 -0500

    fixed /product route and link

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,10 +1,21 @@
-import { Button } from "react-bootstrap";
+'use client'
+import { Button, Navbar, NavbarText, Row } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-      <Button className="btn-danger btn-lg">Products</Button>
-    </div>
-  );
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container > 
+        <Navbar.Brand href="/">E-Commerce Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"> 
+            <Nav.Link href="/products">Products</Nav.Link>
+          </Nav>
+      </Navbar.Collapse>
+      </Container>
+    </Navbar>
+ )
 }
