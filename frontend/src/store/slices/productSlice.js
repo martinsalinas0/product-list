138ce1546def19ed72f826b34e495cac1a52821c
martinsalinas0 <martinlsalinas@pm.me>
2025-06-01 19:03:20 -0500

    made a more simple product slice to only get products

--- frontend/src/store/slices/productSlice.js
+++ frontend/src/store/slices/productSlice.js
@@ -1,45 +1,55 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "http://localhost:8000/api";
+const API_URL = "http://localhost:8000/api"; 
 
-export const fetchProducts = createAsyncThunk(
-  "products/fetchProducts",
-  async (queryParams) => {
-    const response = await axios.get(`${API_URL}/products?${queryParams}`);
-    return response.data;
+
+
+const initialState = { 
+  products: [], 
+  count: null, 
+  page: 1,
+  totalPages: null, 
+  isLoading: false, 
+  error: '' 
+}
+
+
+
+export const fetchProducts = createAsyncThunk( 
+  'products/fetchProducts', 
+  async () => {
+    const response =  await axios.get(`${API_URL}/products`); 
+    return response.data
   }
-);
+); 
 
-const productsSlice = createSlice({
-  name: "products",
-  initialState: {
-    products: [],
-    count: 0,
-    page: 1,
-    totalPages: 1,
-    loading: false,
-    error: null,
-  },
-  reducers: {},
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchProducts.pending, (state) => {
-        state.loading = true;
-        state.error = null;
-      })
-      .addCase(fetchProducts.fulfilled, (state, action) => {
-        state.loading = false;
-        state.products = action.payload.products || [];
-        state.count = action.payload.count || 0;
-        state.page = action.payload.page || 1;
-        state.totalPages = action.payload.totalPages || 1;
-      })
-      .addCase(fetchProducts.rejected, (state, action) => {
-        state.loading = false;
-        state.error = action.error.message || "failed to fetch products";
-      });
-  },
-});
+const productSlice = createSlice({ 
+  name: 'products', 
+  initialState, 
+reducers: {}, 
+extraReducers: (builder) => { 
+  builder.addCase(fetchProducts.pending, (state) => { 
+    state.isLoading = true;
+  }); 
+  builder.addCase(fetchProducts.fulfilled, (state, action) => { 
+    state.isLoading = false; 
+    state.products = action.payload.products || action.payload; 
+    state.count = action.payload.count || action.payload.length;  
+    state.page = action.payload.page || 1
+    state.totalPages = action.payload.totalPages || 1
+    state.error = ''; 
+     
 
-export default productsSlice.reducer;
+  }); 
+  builder.addCase(fetchProducts.rejected, (state, action) => { 
+    state.isLoading = false; 
+    state.error = 'Error fetching request. '
+  })
+}
+
+}); 
+
+export default productSlice.reducer; 
+
+
