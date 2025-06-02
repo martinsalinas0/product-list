8b98cb560b00161eb27bf45737c28fe4cce21283
martinsalinas0 <martinlsalinas@pm.me>
2025-06-01 22:27:11 -0500

    added NavBar.jsx and added it to page,js

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,21 +1,12 @@
-'use client'
-import { Button, Navbar, NavbarText, Row } from "react-bootstrap";
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+"use client";
+import NavBar from "@/components/NavBar";
+import Container from "react-bootstrap/Container";
 
 export default function Home() {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container > 
-        <Navbar.Brand href="/">E-Commerce Store</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto"> 
-            <Nav.Link href="/products">Products</Nav.Link>
-          </Nav>
-      </Navbar.Collapse>
-      </Container>
-    </Navbar>
- )
+    <div>
+      <NavBar> </NavBar>
+      <Container>Click on products above to see all products</Container>
+    </div>
+  );
 }
---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+'use client'
+import { Button, Navbar, NavbarText, Row } from "react-bootstrap";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+export default function NavBar () {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container > 
+        <Navbar.Brand href="/">E-Commerce Store</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" /> 
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto"> 
+            <Nav.Link href="/products">Products</Nav.Link>
+          </Nav>
+      </Navbar.Collapse>
+      </Container>
+      
+    </Navbar>
+  )
+}
